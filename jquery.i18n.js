a90459bca289a0c5b3d0bc80c683f9a6b0d5d210
Michael Weibel <michael.weibel@gmail.com>
2011-11-28 10:39:22 +0100

    build fixed version

--- jquery.i18n.js
+++ jquery.i18n.js
@@ -7,7 +7,7 @@
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
  *
- * Version: 0.9.2 (201111211627)
+ * Version: 0.9.2 (201111281039)
  */
  (function($) {
 /**
